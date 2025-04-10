48d47dcf917e71494ccd073e31b994feb7133d69
مُحَمّد سِيّد <94808133+iskepr@users.noreply.github.com>
2025-04-10 12:51:31 +0200

    تحسينات صفحة الوثائق واضافة صفحة المكتبات

--- Source/JavaScript/script.js
+++ Source/JavaScript/script.js
@@ -13,3 +13,52 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 });
+
+// عرض النافذه
+function openWindow(height) {
+  const winBack = document.querySelector(".winBack");
+  const window = document.querySelector("#window");
+  winBack.onclick = () => {
+    winBack.style.opacity = 0;
+    setTimeout(() => {
+      winBack.style.display = "none";
+    }, 700);
+    setTimeout(() => {
+      window.style.height = "0px";
+    }, 50);
+  };
+  winBack.style.display = "flex";
+  setTimeout(() => {
+    winBack.style.opacity = 1;
+  }, 20);
+  setTimeout(() => {
+    window.style.height = !height ? "100px" : height;
+  }, 50);
+}
+
+// زر النسخ
+function copyToClipboard(text) {
+  navigator.clipboard.writeText(text);
+  notify("تم النسخ الي الحافظة");
+}
+
+// لعرض اشعار
+function notify(text) {
+  const notify = document.createElement("div");
+  notify.className = "notify";
+  notify.textContent = text;
+  document.body.appendChild(notify);
+
+  setTimeout(() => {
+    notify.style.opacity = "1";
+    notify.style.transform = "translateX(0)";
+  }, 10);
+
+  setTimeout(() => {
+    notify.style.opacity = "0";
+    notify.style.transform = "translateX(20px)";
+    setTimeout(() => {
+      document.body.removeChild(notify);
+    }, 500);
+  }, 3000);
+}
